04297098f26216c97042d270b9e08aeb8803f711
Sachith Dissanayaka <e19087@eng.pdn.ac.lk>
2023-09-12 20:48:47 +0530

    navbar updated

--- bloomi_web/lib/controllers/auth_controller.dart
+++ bloomi_web/lib/controllers/auth_controller.dart
@@ -1,6 +1,6 @@
 import 'package:bloomi_web/screens/Admin/adminui.dart';
+import 'package:bloomi_web/screens/Counsellor/navbar.dart';
 import 'package:bloomi_web/screens/auth/signup/signup_form.dart';
-import 'package:bloomi_web/screens/home/Appointment/dashboard.dart';
 import 'package:bloomi_web/screens/home/home/home.dart';
 import 'package:bloomi_web/utils/util_function.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -21,7 +21,7 @@ class AuthController {
         if (user.uid == "3MMoGgwJLVUjpTkJ7f4Zd8FqqyJ2") {
           UtilFunction.navigateForward(context, const Adminpanel());
         } else if (user.uid == "Tm3SJ2JpSORKzZrFiumARyE4l7f1") {
-          UtilFunction.navigateForward(context, const Dashboard());
+          UtilFunction.navigateForward(context, const CounselorNavBar());
         } else {
           UtilFunction.navigateForward(context, const Home());
         }
--- bloomi_web/lib/screens/Counsellor/navbar.dart
+++ bloomi_web/lib/screens/Counsellor/navbar.dart
@@ -1,14 +1,14 @@
 import 'package:bloomi_web/components/custom_text.dart';
+import 'package:bloomi_web/components/footer.dart';
 import 'package:bloomi_web/providers/nav_provider/navigation_provider.dart';
+import 'package:bloomi_web/screens/Counsellor/calender.dart';
 import 'package:bloomi_web/screens/Counsellor/dashboard.dart';
+import 'package:bloomi_web/screens/Counsellor/drawer.dart';
+import 'package:bloomi_web/screens/Counsellor/message.dart';
+import 'package:bloomi_web/screens/Counsellor/profile.dart';
 import 'package:bloomi_web/screens/Counsellor/topbar_contents.dart';
 import 'package:bloomi_web/utils/util_constant.dart';
 import 'package:flutter/material.dart';
-import 'package:bloomi_web/screens/Counsellor/drawer.dart';
-import 'package:bloomi_web/screens/Counsellor/calender.dart';
-import 'package:bloomi_web/screens/Counsellor/message.dart';
-import 'package:bloomi_web/screens/Counsellor/profile.dart';
-import 'package:bloomi_web/components/footer.dart';
 import 'package:provider/provider.dart';
 
 class CounselorNavBar extends StatefulWidget {
@@ -51,6 +51,7 @@ class _CounselorNavBarState extends State<CounselorNavBar> {
     );*/
 
     return Scaffold(
+      bottomNavigationBar: Footer(mediaQueryData: mediaQueryData),
       appBar: (screenSize.width <= 900)
           ? AppBar(
               iconTheme: const IconThemeData(color: UtilConstants.primaryColor),
