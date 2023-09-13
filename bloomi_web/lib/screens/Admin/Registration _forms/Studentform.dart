1a6cb6776b766a1271f0c16d7de97528b6ff2c18
akilagn <115577155+akilagn@users.noreply.github.com>
2023-09-14 00:29:28 +0530

    Student form updated

--- bloomi_web/lib/screens/Admin/Registration _forms/Studentform.dart
+++ bloomi_web/lib/screens/Admin/Registration _forms/Studentform.dart
@@ -1,10 +1,10 @@
 import 'package:bloomi_web/components/background_color_gradient.dart';
+import 'package:bloomi_web/components/custom_text.dart';
 import 'package:bloomi_web/components/dropdown_button.dart';
 import 'package:bloomi_web/components/form_button_web.dart';
 import 'package:bloomi_web/components/form_heading.dart';
 import 'package:bloomi_web/components/form_input_web.dart';
 import 'package:bloomi_web/providers/auth/signup_provider.dart';
-import 'package:bloomi_web/utils/util_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,9 +13,7 @@ class Studentform extends StatefulWidget {
     super.key,
     required this.mediaQueryData,
   });
-
   final MediaQueryData mediaQueryData;
-
   @override
   State<Studentform> createState() => _StudentformState();
 }
@@ -41,156 +39,120 @@ class _StudentformState extends State<Studentform> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        children: [
-          const BackgroundColorGradient(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 200),
+          child: Column(
             children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: widget.mediaQueryData.size.width * 0.1),
+              SizedBox(height: mediaQueryData.size.height * 0.02),
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    margin:
+                        EdgeInsets.only(top: mediaQueryData.size.height * 0.02),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
                     child: Column(
                       children: [
-                        SizedBox(
-                            height: widget.mediaQueryData.size.height * 0.02),
-                        Row(
-                          children: [
-                            // CustomImageColumn(
-                            //     mediaQueryData: widget.mediaQueryData),
-                            // SizedBox(
-                            //     width: widget.mediaQueryData.size.width * 0.12),
-                            Container(
-                              padding: EdgeInsets.all(
-                                  widget.mediaQueryData.size.width * 0.03),
-                              margin: EdgeInsets.only(
-                                  top:
-                                      widget.mediaQueryData.size.height * 0.02),
-                              decoration: BoxDecoration(
-                                color: UtilConstants.lightgreyColor,
-                                borderRadius: BorderRadius.circular(
-                                    widget.mediaQueryData.size.height * 0.03),
-                              ),
-                              child: Column(
-                                children: [
-                                  FormHeading("Register Students",
-                                      mediaQueryData: widget.mediaQueryData),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.04),
-                                  FormInputWeb("Name",
-                                      textEditingController:
-                                          Provider.of<SignupProvider>(context)
-                                              .name,
-                                      mediaQueryData: widget.mediaQueryData),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.01),
-                                  FormInputWeb("Email",
-                                      textEditingController:
-                                          Provider.of<SignupProvider>(context)
-                                              .email,
-                                      mediaQueryData: widget.mediaQueryData),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.01),
-                                  FormInputWeb(
-                                    "Password",
-                                    textEditingController:
-                                        Provider.of<SignupProvider>(context)
-                                            .password,
-                                    mediaQueryData: widget.mediaQueryData,
-                                    obscure: true,
-                                  ),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.01),
-                                  FormInputWeb("Phone Number",
-                                      textEditingController:
-                                          Provider.of<SignupProvider>(context)
-                                              .phoneNumber,
-                                      mediaQueryData: widget.mediaQueryData),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.01),
-                                  CustomDropdownMenu(
-                                    "Faculty",
-                                    listItem: listItems,
-                                    textEditingController:
-                                        Provider.of<SignupProvider>(context)
-                                            .faculty,
-                                  ),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.01),
-                                  FormInputWeb("Department",
-                                      textEditingController:
-                                          Provider.of<SignupProvider>(context)
-                                              .department,
-                                      mediaQueryData: widget.mediaQueryData),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.01),
-                                  CustomDropdownMenu(
-                                    "Level of Study",
-                                    listItem: year,
-                                    textEditingController:
-                                        Provider.of<SignupProvider>(context)
-                                            .year,
-                                  ),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.06),
-                                  Consumer<SignupProvider>(
-                                    builder: (context, value, child) {
-                                      return InkWell(
-                                        onTap: () {
-                                          Provider.of<SignupProvider>(context,
-                                                  listen: false)
-                                              .signUpUser(
-                                                  value.email.text,
-                                                  value.password.text,
-                                                  widget.mediaQueryData,
-                                                  context);
-                                        },
-                                        child: FormButtonWeb(
-                                          "Register",
-                                          mediaQueryData: widget.mediaQueryData,
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  SizedBox(
-                                      height:
-                                          widget.mediaQueryData.size.height *
-                                              0.02),
-                                ],
-                              ),
-                            ),
-                          ],
+                        FormHeading("Register Students",
+                            mediaQueryData: mediaQueryData),
+                        SizedBox(height: mediaQueryData.size.height * 0.04),
+                        FormInputWeb("Name",
+                            textEditingController:
+                                Provider.of<SignupProvider>(context).name,
+                            mediaQueryData: mediaQueryData),
+                        SizedBox(height: mediaQueryData.size.height * 0.01),
+                        FormInputWeb("Email",
+                            textEditingController:
+                                Provider.of<SignupProvider>(context).email,
+                            mediaQueryData: mediaQueryData),
+                        SizedBox(height: mediaQueryData.size.height * 0.01),
+                        FormInputWeb(
+                          "Password",
+                          textEditingController:
+                              Provider.of<SignupProvider>(context).password,
+                          mediaQueryData: mediaQueryData,
+                          obscure: true,
                         ),
-                        SizedBox(
-                          height: widget.mediaQueryData.size.height * 0.1,
+                        SizedBox(height: mediaQueryData.size.height * 0.01),
+                        FormInputWeb("Phone Number",
+                            textEditingController:
+                                Provider.of<SignupProvider>(context)
+                                    .phoneNumber,
+                            mediaQueryData: mediaQueryData),
+                        SizedBox(height: mediaQueryData.size.height * 0.01),
+                        CustomDropdownMenu(
+                          "Faculty",
+                          listItem: listItems,
+                          textEditingController:
+                              Provider.of<SignupProvider>(context).faculty,
                         ),
+                        SizedBox(height: mediaQueryData.size.height * 0.01),
+                        FormInputWeb("Department",
+                            textEditingController:
+                                Provider.of<SignupProvider>(context).department,
+                            mediaQueryData: mediaQueryData),
+                        SizedBox(height: mediaQueryData.size.height * 0.01),
+                        CustomDropdownMenu(
+                          "Level of Study",
+                          listItem: year,
+                          textEditingController:
+                              Provider.of<SignupProvider>(context).year,
+                        ),
+                        SizedBox(height: mediaQueryData.size.height * 0.06),
+                        Consumer<SignupProvider>(
+                          builder: (context, value, child) {
+                            return InkWell(
+                              onTap: () {
+                                Provider.of<SignupProvider>(context,
+                                        listen: false)
+                                    .signUpUser(
+                                        value.email.text,
+                                        value.password.text,
+                                        mediaQueryData,
+                                        context);
+                              },
+                              child: FormButtonWeb(
+                                "Register",
+                                mediaQueryData: mediaQueryData,
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(height: mediaQueryData.size.height * 0.02),
                       ],
                     ),
                   ),
-                ),
+                ],
+              ),
+              SizedBox(
+                height: mediaQueryData.size.height * 0.1,
+              ),
+              Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white,
+                      elevation: 100,
+                      padding: const EdgeInsets.all(20),
+                    ),
+                    child: const Text(
+                      'Go Back',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    )),
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
