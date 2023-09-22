ddc56a4987b7398734b97aa9e2fffcc8dcf90b97
akilagn <115577155+akilagn@users.noreply.github.com>
2023-09-22 22:20:38 +0530

    Update admin_control.dart

--- bloomi_web/lib/screens/admin_screens/admin_table/admin_control.dart
+++ bloomi_web/lib/screens/admin_screens/admin_table/admin_control.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
-import 'package:bloomi_web/components/custom_tablecells.dart';
-import 'package:bloomi_web/components/custom_tableheads.dart';
 import 'package:bloomi_web/providers/user_home_provider/user_appoinment_provider.dart';
 import 'package:bloomi_web/screens/admin_screens/registration%20_forms/adminform.dart';
 import 'package:bloomi_web/utils/util_constant.dart';
@@ -27,158 +25,207 @@ class _AdminControlState extends State<AdminControl> {
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    var width = mediaQueryData.size.width;
+
     return Scaffold(
-      backgroundColor: Colors.blue.shade50,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-              child: ElevatedButton(
-                onPressed: () {
-                  UtilFunction.navigateForward(
-                    context,
-                    const AdminRegistrationForm(),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  elevation: 5,
-                  padding: const EdgeInsets.all(20),
-                ),
-                child: const Text(
-                  'Add Admins',
-                  style: TextStyle(
-                    fontSize: 20,
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    UtilFunction.navigateForward(
+                      context,
+                      const AdminRegistrationForm(),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    padding: const EdgeInsets.all(20),
+                  ),
+                  child: const Text(
+                    'Add Admins',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Consumer<UserAppoinmentProvider>(
-              builder: (context, value, child) {
-                // Initialize the lists based on the length of allAdminModel
-                isRowHovered =
-                    List.generate(value.allAdminModel.length, (index) => false);
-                isRowHoveredDeleted =
-                    List.generate(value.allAdminModel.length, (index) => false);
+              Consumer<UserAppoinmentProvider>(
+                builder: (context, value, child) {
+                  // Initialize the lists based on the length of allAdminModel
+                  isRowHovered = List.generate(
+                      value.allAdminModel.length, (index) => false);
+                  isRowHoveredDeleted = List.generate(
+                      value.allAdminModel.length, (index) => false);
 
-                return Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Table(
-                    columnWidths: const <int, TableColumnWidth>{
-                      0: FlexColumnWidth(1),
-                      1: FlexColumnWidth(1),
-                      2: FlexColumnWidth(1.1),
-                      3: FlexColumnWidth(0.8),
-                      4: FlexColumnWidth(1.2),
-                      5: FlexColumnWidth(1.2),
-                    },
-                    border: TableBorder.all(
-                      width: 1.5,
-                      color: UtilConstants.primaryColor,
-                    ),
-                    children: [
-                      const TableRow(children: [
-                        Tableheads(text: 'Admin Id'),
-                        Tableheads(text: 'Name'),
-                        Tableheads(text: 'Email'),
-                        Tableheads(text: 'Contact Nu.'),
-                        // Tableheads(text: 'Faculty'),
-                        Tableheads(text: 'Credentials'),
-                        Tableheads(text: 'Actions'),
-                      ]),
-                      ...List.generate(
-                        value.allAdminModel.length,
-                        (index) => TableRow(children: [
-                          Tablecellwidget(name: value.allAdminModel[index].uid),
-                          Tablecellwidget(
-                              name: value.allAdminModel[index].name),
-                          Tablecellwidget(
-                              name: value.allAdminModel[index].email),
-                          Tablecellwidget(
-                              name: value.allAdminModel[index].phone),
-                          // Tablecellwidget(name: faculties[index]),
-                          Tablecellwidget(
-                              name: value.allAdminModel[index].userCredential),
-                          TableCell(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  MouseRegion(
-                                    onEnter: (_) {
-                                      setState(() {
-                                        isRowHovered[index] = true;
-                                      });
-                                    },
-                                    onExit: (_) {
-                                      setState(() {
-                                        isRowHovered[index] = false;
-                                      });
-                                    },
-                                    child: ElevatedButton(
-                                        onPressed: () {
-                                          UtilFunction.navigateForward(
-                                            context,
-                                            const AdminRegistrationForm(),
-                                          );
-                                        },
-                                        style: ElevatedButton.styleFrom(
-                                          backgroundColor: isRowHovered[index]
-                                              ? Colors.greenAccent
-                                              : Colors.grey.shade50,
-                                        ),
-                                        child: Text(
-                                          'Update',
-                                          style: TextStyle(
-                                            color: isRowHovered[index]
-                                                ? Colors.black
-                                                : Colors.purple.shade400,
-                                          ),
-                                        )),
+                  return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: DataTable(
+                          dataRowMinHeight: 50,
+                          dataRowMaxHeight: 50,
+                          dataTextStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                          columnSpacing: (width * 1 / 20),
+                          columns: [
+                            DataColumn(
+                                label: Center(
+                              child: Text(
+                                'Admin Id',
+                                style: TextStyle(
+                                    color: UtilConstants.greenColor,
+                                    fontSize: 20),
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Name',
+                              style: TextStyle(
+                                  color: UtilConstants.greenColor,
+                                  fontSize: 20),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Email',
+                              style: TextStyle(
+                                  color: UtilConstants.greenColor,
+                                  fontSize: 20),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Contact Number',
+                              style: TextStyle(
+                                  color: UtilConstants.greenColor,
+                                  fontSize: 20),
+                            )),
+                            // Tableheads(text: 'Faculty'),
+                            DataColumn(
+                                label: Text(
+                              'Faculty',
+                              style: TextStyle(
+                                  color: UtilConstants.greenColor,
+                                  fontSize: 20),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Actions',
+                              style: TextStyle(
+                                  color: UtilConstants.greenColor,
+                                  fontSize: 20),
+                            )),
+                          ],
+                          rows: List.generate(
+                            value.allAdminModel.length,
+                            (index) {
+                              return DataRow(
+                                cells: [
+                                  DataCell(
+                                    Text(
+                                      value.allAdminModel[index].uid,
+                                    ),
                                   ),
-                                  MouseRegion(
-                                    onEnter: (_) {
-                                      setState(() {
-                                        isRowHoveredDeleted[index] = true;
-                                      });
-                                    },
-                                    onExit: (_) {
-                                      setState(() {
-                                        isRowHoveredDeleted[index] = false;
-                                      });
-                                    },
-                                    child: ElevatedButton(
-                                      onPressed: () {},
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor:
-                                            isRowHoveredDeleted[index]
-                                                ? Colors.redAccent.shade200
-                                                : Colors.grey.shade50,
-                                      ),
-                                      child: Text(
-                                        'Delete',
-                                        style: TextStyle(
-                                          color: isRowHoveredDeleted[index]
-                                              ? Colors.black
-                                              : Colors.purple.shade400,
-                                        ),
+                                  DataCell(
+                                      Text(value.allAdminModel[index].name)),
+                                  DataCell(
+                                      Text(value.allAdminModel[index].email)),
+                                  DataCell(
+                                      Text(value.allAdminModel[index].phone)),
+                                  DataCell(Text(value
+                                      .allAdminModel[index].userCredential)),
+                                  DataCell(
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 10),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          MouseRegion(
+                                            onEnter: (_) {
+                                              setState(() {
+                                                isRowHovered[index] = true;
+                                              });
+                                            },
+                                            onExit: (_) {
+                                              setState(() {
+                                                isRowHovered[index] = false;
+                                              });
+                                            },
+                                            child: ElevatedButton(
+                                                onPressed: () {
+                                                  UtilFunction.navigateForward(
+                                                    context,
+                                                    const AdminRegistrationForm(),
+                                                  );
+                                                },
+                                                style: ElevatedButton.styleFrom(
+                                                  backgroundColor:
+                                                      isRowHovered[index]
+                                                          ? Colors.greenAccent
+                                                          : Colors.grey.shade50,
+                                                ),
+                                                child: Text(
+                                                  'Update',
+                                                  style: TextStyle(
+                                                    color: isRowHovered[index]
+                                                        ? Colors.black
+                                                        : Colors
+                                                            .purple.shade400,
+                                                  ),
+                                                )),
+                                          ),
+                                          MouseRegion(
+                                            onEnter: (_) {
+                                              setState(() {
+                                                isRowHoveredDeleted[index] =
+                                                    true;
+                                              });
+                                            },
+                                            onExit: (_) {
+                                              setState(() {
+                                                isRowHoveredDeleted[index] =
+                                                    false;
+                                              });
+                                            },
+                                            child: ElevatedButton(
+                                              onPressed: () {},
+                                              style: ElevatedButton.styleFrom(
+                                                backgroundColor:
+                                                    isRowHoveredDeleted[index]
+                                                        ? Colors
+                                                            .redAccent.shade200
+                                                        : Colors.grey.shade50,
+                                              ),
+                                              child: Text(
+                                                'Delete',
+                                                style: TextStyle(
+                                                  color:
+                                                      isRowHoveredDeleted[index]
+                                                          ? Colors.black
+                                                          : Colors
+                                                              .purple.shade400,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
                                   ),
                                 ],
-                              ),
-                            ),
-                          ),
-                        ]),
-                      )
-                    ],
-                  ),
-                );
-              },
-            )
-          ],
+                              );
+                            },
+                          )));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
