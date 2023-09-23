2c3297eb031691b39432bacaf177c127764416fc
Sachith Dissanayaka <e19087@eng.pdn.ac.lk>
2023-09-23 20:59:21 +0530

    solve error

--- bloomi_web/lib/screens/admin_screens/registration _forms/adminform.dart
+++ bloomi_web/lib/screens/admin_screens/registration _forms/adminform.dart
@@ -1,4 +1,5 @@
 // Admin Register Form
+
 import 'package:bloomi_web/components/form_button_web.dart';
 import 'package:bloomi_web/components/form_heading.dart';
 import 'package:bloomi_web/components/form_input_web.dart';
@@ -6,89 +7,94 @@ import 'package:bloomi_web/providers/admin/admin_registration_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-Future<void> adminregistrationform(BuildContext context) {
-  return showDialog<void>(
-    context: context,
-    builder: (BuildContext context) {
-      double heightone = MediaQuery.of(context).size.height;
-      return Center(
-        child: Container(
-          width: 500,
-          height: 1000,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(2.0),
-          ),
-          child: AlertDialog(
-            actions: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: Center(
-                  child: FormHeading(
-                    "Add Admins",
-                    color: Colors.grey,
+class AdminForm {
+  //----------------------admin registration form-----------------------
+  static adminregistrationform(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        double heightone = MediaQuery.of(context).size.height;
+        return Center(
+          child: Container(
+            width: 500,
+            height: 1000,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(2.0),
+            ),
+            child: AlertDialog(
+              actions: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Center(
+                    child: FormHeading(
+                      "Add Admins",
+                      color: Colors.grey,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: heightone * 0.04),
-              FormInputWeb(
-                "Name",
-                textEditingController:
-                    Provider.of<AdminRegistrationProvider>(context).name,
-              ),
-              SizedBox(height: heightone * 0.01),
-              FormInputWeb(
-                "Email",
-                textEditingController:
-                    Provider.of<AdminRegistrationProvider>(context).email,
-              ),
-              SizedBox(height: heightone * 0.01),
-              FormInputWeb(
-                "Password",
-                obscure: true,
-                textEditingController:
-                    Provider.of<AdminRegistrationProvider>(context).password,
-              ),
-              SizedBox(height: heightone * 0.01),
-              FormInputWeb(
-                "Phone Number",
-                textEditingController:
-                    Provider.of<AdminRegistrationProvider>(context).phoneNumber,
-              ),
-              SizedBox(height: heightone * 0.01),
-              SizedBox(height: heightone * 0.01),
-              FormInputWeb(
-                "Credentials",
-                textEditingController:
-                    Provider.of<AdminRegistrationProvider>(context).credentials,
-              ),
-              SizedBox(height: heightone * 0.01),
-              SizedBox(height: heightone * 0.01),
-              SizedBox(height: heightone * 0.01),
-              Consumer<AdminRegistrationProvider>(
-                builder: (context, value, child) {
-                  return InkWell(
-                    onTap: () {
-                      value.signUpUser(
-                          value.name.text,
-                          value.email.text,
-                          value.password.text,
-                          value.phoneNumber.text,
-                          // value.faculty,
-                          value.credentials.text,
-                          context);
-                    },
-                    child: FormButtonWeb(
-                      "Register",
-                      isLoading: value.isLoading,
-                    ),
-                  );
-                },
-              ),
-              SizedBox(height: heightone * 0.02),
-            ],
+                SizedBox(height: heightone * 0.04),
+                FormInputWeb(
+                  "Name",
+                  textEditingController:
+                      Provider.of<AdminRegistrationProvider>(context).name,
+                ),
+                SizedBox(height: heightone * 0.01),
+                FormInputWeb(
+                  "Email",
+                  textEditingController:
+                      Provider.of<AdminRegistrationProvider>(context).email,
+                ),
+                SizedBox(height: heightone * 0.01),
+                FormInputWeb(
+                  "Password",
+                  obscure: true,
+                  textEditingController:
+                      Provider.of<AdminRegistrationProvider>(context).password,
+                ),
+                SizedBox(height: heightone * 0.01),
+                FormInputWeb(
+                  "Phone Number",
+                  textEditingController:
+                      Provider.of<AdminRegistrationProvider>(context)
+                          .phoneNumber,
+                ),
+                SizedBox(height: heightone * 0.01),
+                SizedBox(height: heightone * 0.01),
+                FormInputWeb(
+                  "Credentials",
+                  textEditingController:
+                      Provider.of<AdminRegistrationProvider>(context)
+                          .credentials,
+                ),
+                SizedBox(height: heightone * 0.01),
+                SizedBox(height: heightone * 0.01),
+                SizedBox(height: heightone * 0.01),
+                Consumer<AdminRegistrationProvider>(
+                  builder: (context, value, child) {
+                    return InkWell(
+                      onTap: () {
+                        value.signUpUser(
+                            value.name.text,
+                            value.email.text,
+                            value.password.text,
+                            value.phoneNumber.text,
+                            // value.faculty,
+                            value.credentials.text,
+                            context);
+                      },
+                      child: FormButtonWeb(
+                        "Register",
+                        isLoading: value.isLoading,
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: heightone * 0.02),
+              ],
+            ),
           ),
-        ),
-      );
-    },
-  );
+        );
+      },
+    );
+  }
 }
