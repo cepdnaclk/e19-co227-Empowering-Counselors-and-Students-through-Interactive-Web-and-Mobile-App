16969174699b67d751ed5bf1099316e36b03f33d
akilagn <115577155+akilagn@users.noreply.github.com>
2023-09-23 19:54:35 +0530

    Custom Control  button created

---
+++ bloomi_web/lib/components/custom_control_buttons.dart
@@ -0,0 +1,35 @@
+import 'package:bloomi_web/screens/admin_screens/registration%20_forms/adminform.dart';
+import 'package:flutter/material.dart';
+
+class CustomControlButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Color basiccolor;
+  const CustomControlButton({
+    super.key,
+    required this.text,
+    required this.color,
+    required this.basiccolor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      child: ElevatedButton(
+          onPressed: () => adminregistrationform(context),
+          style: ButtonStyle(
+            overlayColor: MaterialStateProperty.resolveWith<Color?>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.hovered)) {
+                  return color; //<-- SEE HERE
+                }
+                return basiccolor; // Defer to the widget's default.
+              },
+            ),
+          ),
+          child: Text(
+            text,
+          )),
+    );
+  }
+}
