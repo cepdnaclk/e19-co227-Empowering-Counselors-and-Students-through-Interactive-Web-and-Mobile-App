f8d298c2986b5359bfe0fedd156f9e4416257388
akilagn <115577155+akilagn@users.noreply.github.com>
2023-10-29 00:00:15 +0530

    updated admin dashboard

--- bloomi_web/lib/screens/admin_screens/home/admin_home.dart
+++ bloomi_web/lib/screens/admin_screens/home/admin_home.dart
@@ -159,7 +159,7 @@ class _AdminHomeState extends State<AdminHome> {
           titleStyle: TextStyle(
             fontSize: fontSize,
             fontWeight: FontWeight.bold,
-            color: Colors.white,
+            color: Color.fromARGB(255, 32, 32, 32),
           ),
           radius: radius,
         );
